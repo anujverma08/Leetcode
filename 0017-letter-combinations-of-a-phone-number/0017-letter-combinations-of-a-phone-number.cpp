e48595062b5a5a13906c005f1c0379233da44ccb
Anuj <anujvermagkp@gmail.com>
2025-08-27 14:55:48 +0530

    Time: 0 ms (100%), Space: 9.2 MB (75.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    void solve(int index, vector<string>& res, string& sb, 
-               const string& input, unordered_map<char, string>& map) {
+    void solve(int index, vector<string>& res, string& sb, const string& input,
+               unordered_map<char, string>& map) {
         if (sb.size() == input.size()) {
             res.push_back(sb);
             return;
@@ -13,14 +13,13 @@ public:
             sb.pop_back();
         }
     }
-    
+
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
-        
+        if (digits.empty())
+            return {};
         unordered_map<char, string> map = {
-            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
-            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
-        };
+            {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
+            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
         vector<string> res;
         string sb;
         solve(0, res, sb, digits, map);
