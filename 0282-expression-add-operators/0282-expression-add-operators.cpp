78dfc048e13398b0464836ca5619fab2accf8e8f
Anuj <anujvermagkp@gmail.com>
2025-08-28 19:36:10 +0530

    Time: 395 ms (77.7%), Space: 92.2 MB (81.77%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void solve(int index, vector<string>& res, string expr, long long prev, long long curr, int& target, string& num){
+        if(index == num.size()){
+            if(curr == target){
+                res.push_back(expr);
+                
+            }
+            return;
+        }
+        string temp="";
+        long long currNum = 0;
+        for(int i = index; i < num.size(); i++){
+            temp += num[i];
+            currNum = currNum * 10 + (num[i] - '0');
+
+            if(i > index && num[index] == '0') break;
+
+            if(index == 0){
+                solve(i + 1,res,temp,currNum,currNum,target,num);
+            }else{
+                solve(i + 1,res,expr+'+'+temp,currNum,curr+currNum,target,num);
+                solve(i + 1,res,expr+'-'+temp,-currNum,curr-currNum,target,num);
+                solve(i + 1,res,expr+'*'+temp,currNum * prev,curr-prev + (currNum * prev),target,num);
+            }
+        }
+    }
+    vector<string> addOperators(string num, int target) {
+        vector<string> res;
+        solve(0,res,"",0,0,target,num);
+        return res;
+    }
+};
