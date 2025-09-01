dc14fd310169e69de7ecb441c6b31fee0b9a648e
Anuj <anujvermagkp@gmail.com>
2025-09-02 00:43:52 +0530

    Time: 416 ms (39.67%), Space: 211.5 MB (42.35%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    int solve(int index, int buy, int cap, vector<int>& prices,
-              vector<vector<vector<int>>>& dp) {
-        if (index == prices.size() || cap == 0)
-            return 0;
-
-        if (dp[index][buy][cap] != -1)
-            return dp[index][buy][cap];
-
-        if (buy) {
-            int profit = -prices[index] + solve(index + 1, 0, cap, prices, dp);
-            profit = max(profit, solve(index + 1, 1, cap, prices, dp));
-            return dp[index][buy][cap] = profit;
-        } else {
-            int profit =
-                prices[index] + solve(index + 1, 1, cap - 1, prices, dp);
-            profit = max(profit, solve(index + 1, 0, cap, prices, dp));
-            return dp[index][buy][cap] = profit;
-        }
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solve(0, 1, 2, prices, dp);
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= 1; j++) {
+                dp[i][j][0] = 0;
+            }
+        }
+        for (int j = 0; j <= 1; j++) {
+            for (int k = 0; k < 3; k++) {
+                dp[n][j][k] = 0;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 1; k <= 2; k++) {
+                    if (j) {
+                        int profit = -prices[i] + dp[i + 1][0][k];
+                        profit = max(profit, dp[i + 1][1][k]);
+                        dp[i][j][k] = profit;
+                    } else {
+                        int profit = prices[i] + dp[i + 1][1][k - 1];
+                        profit = max(profit, dp[i + 1][0][k]);
+                        dp[i][j][k] = profit;
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
