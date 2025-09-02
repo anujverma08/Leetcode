f357dce743e5d2358b6acb360d9cb8da5b8d075f
Anuj <anujvermagkp@gmail.com>
2025-09-02 17:59:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
-        if(i > j) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
+    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i > j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         int maxi = INT_MIN;
-        for(int ind = i; ind <= j; ind++){
-            int curr =  nums[i - 1] * nums[ind] * nums[j + 1];
-            int res = solve(i , ind - 1, nums, dp) + solve(ind + 1, j, nums, dp);
+        for (int ind = i; ind <= j; ind++) {
+            int curr = nums[i - 1] * nums[ind] * nums[j + 1];
+            int res = solve(i, ind - 1, nums, dp) + solve(ind + 1, j, nums, dp);
             maxi = max(maxi, res + curr);
         }
         return dp[i][j] = maxi;
-;
+        ;
     }
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
-        return solve(1, n , nums, dp);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        for (int i = ns; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j)
+                    continue;
+                int maxi = INT_MIN;
+                for (int ind = i; ind <= j; ind++) {
+                    int res = nums[i - 1] * nums[ind] * nums[j + 1] +
+                              dp[i][ind - 1] + dp[ind + 1][j];
+                    maxi = max(maxi, res);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n];
     }
 };
